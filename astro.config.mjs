e17aa0d8641a9f6775ab5e6a07c82ad8deef1022
yw2544 <yw2544@cornell.edu>
2025-12-02 02:14:12 -0500

    fix display

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://enact-embodied-cognition.github.io",
-    base: '/',
+    site: 'https://yw2544.github.io/Theory-of-Space.github.io/',
+    base: '/Theory-of-Space.github.io/'
 });
